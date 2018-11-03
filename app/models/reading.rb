9d826ec70dbcc79a6c1df85245425054e55df218
Hiroyuki Sasagawa <hs19870702@gmail.com>
2018-11-03 19:03:49 +0900

    Format reading json

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,4 +1,9 @@
 class Reading < ApplicationRecord
   belongs_to :thermostat, foreign_key: :thermostats_id
   THERMOSTAT_ATTRIBUTES = [:temperature, :humidity, :battery_charge]
+
+  def as_json(options = {})
+    options[:except] ||= [:id, :created_at, :updated_at]
+    super(options)
+  end
 end
